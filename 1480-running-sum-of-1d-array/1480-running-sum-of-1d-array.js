8d30dc26513531abd3074c75b133a9d5b7b27502
Di Daan Truong <48251225+djdaantruong@users.noreply.github.com>
2022-11-10 15:25:45 +0700

    Time: 110 ms (27.79%), Space: 42.3 MB (70.73%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -2,17 +2,12 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+
 var runningSum = function(nums) {
-    if(nums.length==0) return [];
-    
-    const sumArray = [];
-    
-    let currentSum = nums[0];
-    sumArray.push(currentSum)
-    
-    for(let i = 1; i<nums.length; i++){
-        currentSum+=nums[i]
-        sumArray.push(currentSum)
-    }
-    return sumArray;
+    let ans = new Array(nums.length)
+    ans[0] = nums[0]
+    for (let i = 1; i < nums.length; i++)
+        ans[i] = ans[i-1] + nums[i]
+    return ans
 };
+
