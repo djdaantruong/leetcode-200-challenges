733490abab39ff3f6ea06ac8450f7136938d9e64
Di Daan Truong <48251225+djdaantruong@users.noreply.github.com>
2022-11-19 23:49:31 +0700

    Time: 1697 ms (12.45%), Space: 48.3 MB (94.73%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = (nums) => {
+    for (let right = 0; right < nums.length; right++) {
+        for (let left = 0; left < right; left++) {
+            const isDuplicate = nums[left] === nums[right];
+            if (isDuplicate) return true;
+        }
+    }
+
+    return false;
+}
